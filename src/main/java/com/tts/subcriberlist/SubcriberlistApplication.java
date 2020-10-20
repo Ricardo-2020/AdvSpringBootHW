d753449a8333d9fc4fd47e165d3cc1f91adbba61
Ricardo P <ricardopaucar1363@gmail.com>
2020-10-19 23:51:48 -0500

    init commit

---
+++ src/main/java/com/tts/subcriberlist/SubcriberlistApplication.java
@@ -0,0 +1,13 @@
+package com.tts.subcriberlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubcriberlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SubcriberlistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/subcriberlist/SubcriberlistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.subcriberlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubcriberlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
